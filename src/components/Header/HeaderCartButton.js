2ae8ea8edd2f24851ca24196c28fdb676d1990e1
Fahd Jibril <jibreelfahd@gmail.com>
2024-08-12 21:59:20 +0100

    small edits

--- src/components/Header/HeaderCartButton.js
+++ src/components/Header/HeaderCartButton.js
@@ -3,7 +3,7 @@ import styles from "./HeaderCartButton.module.css";
 const HeaderCartButton = ({ onClickBg }) => {
   return (
     <button className={styles["header--cart"]} onClick={onClickBg}>
-      <span className={styles.cart}>My Cart</span>
+      <span>My Cart</span>
       <span className={styles.badge}>1</span>
     </button>
   );
